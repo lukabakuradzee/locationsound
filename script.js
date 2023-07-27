82abab712b2c0370e864ee7a94984253ac2357ae
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-07-27 21:39:00 +0400

    page loader remove

--- script.js
+++ script.js
@@ -22,35 +22,35 @@ function  setupMobileMenu () {
 
 
 
-function setupIntersectionObserver () {
-  const sections = document.querySelectorAll(".section1,.section_capabilities, .section_our_work, .footer1,.section1_about, .section6_aboutus_home, .section_meet_our_team, .section1_services, .section2_services, .article_service, .section1_portfolio, .section2_portfolio, .section1_contact, .section2_contact");
+// function setupIntersectionObserver () {
+//   const sections = document.querySelectorAll(".section1,.section_capabilities, .section_our_work, .footer1,.section1_about, .section6_aboutus_home, .section_meet_our_team, .section1_services, .section2_services, .article_service, .section1_portfolio, .section2_portfolio, .section1_contact, .section2_contact");
 
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.10, 
-};
+// const options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.10, 
+// };
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("load");
-      observer.unobserve(entry.target);
-    }
-  });
-}, options);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("load");
+//       observer.unobserve(entry.target);
+//     }
+//   });
+// }, options);
 
-sections.forEach((section) => {
-  observer.observe(section);
-});
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
 
 
-}
+// }
 
 
 document.addEventListener("DOMContentLoaded", function () {
   setupMobileMenu();
-  setupIntersectionObserver();
+  // setupIntersectionObserver();
 });
 
   
