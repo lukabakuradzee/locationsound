e54881a04c74230668c131c4e72d61ff568f37e2
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-07-27 21:21:05 +0400

    added content loader , removed contact information from footer

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-let mobileMenu = document.querySelector(".menu")
-let burgerMenu = document.querySelector(".burgermenu")
-let xmark = document.querySelector(".xmark")
+"use strict";
 
-burgerMenu.addEventListener("click", () => {
-  mobileMenu.classList.add("show");
-  burgerMenu.style.display = "none";
-  xmark.style.display = "block";
+
+function  setupMobileMenu () {
+  let MobileMenu = document.querySelector('.menu');
+  let burgerMenu = document.querySelector('.burgermenu');
+  let xmark = document.querySelector('.xmark');
+
+  burgerMenu.addEventListener('click', () => {
+    MobileMenu.classList.add('show');
+    burgerMenu.style.display = 'none';
+    xmark.style.display = 'block';
+  });
+
+  xmark.addEventListener('click', () => {
+    MobileMenu.classList.remove('show');
+    burgerMenu.style.display = 'block';
+    xmark.style.display = 'none';
+  });
+
+}
+
+
+
+function setupIntersectionObserver () {
+  const sections = document.querySelectorAll(".section1,.section_capabilities, .section_our_work, .footer1,.section1_about, .section6_aboutus_home, .section_meet_our_team, .section1_services, .section2_services, .article_service, .section1_portfolio, .section2_portfolio, .section1_contact, .section2_contact");
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.10, 
+};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("load");
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
 
-xmark.addEventListener("click", () => {
-  mobileMenu.classList.remove("show");
-  burgerMenu.style.display = "block";
-  xmark.style.display = "none";
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupMobileMenu();
+  setupIntersectionObserver();
 });
 
-
-// document.addEventListener('DOMContentLoaded', () => {
   
-//   const form = document.getElementById('myForm');
 
-  
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault(); 
-//   });
-// });
 
 
 
