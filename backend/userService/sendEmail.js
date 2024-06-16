ca5fc26548e71d6b33c189e68c7e68ef2e74d40b
lukabakuradzee <lukabakuradzee@gmail.com>
2024-06-16 12:28:51 +0400

    sendemail & updated text color

--- backend/userService/sendEmail.js
+++ backend/userService/sendEmail.js
@@ -19,7 +19,7 @@ const sendEmail = async (req, res) => {
       html: `
         <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
         <div style="max-width: 600px; margin: auto; border: 1px solid #ddd; padding: 20px; border-radius: 5px;">
-          <img src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/416841832_893621152769709_750835283606735095_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEd4KCPqAenl6Hu9b5cp1ICwzbgnXdpwaPDNuCdd2nBo3NtpGEqd9rDE52e3pxYHpysFmwsIG7g3Dcz-mMjmRty&_nc_ohc=HdVZjXoCKfoQ7kNvgFEzR9a&_nc_ht=scontent.ftbs5-3.fna&oh=00_AYB6SkxzugX5c_ioKSs-kmrxCo7NsLvGA3hyy2muhLTZgQ&oe=6673C3D7" alt="Logo" style="max-width: 250px; margin-bottom: 20px;">
+          <img src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/416841832_893621152769709_750835283606735095_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEd4KCPqAenl6Hu9b5cp1ICwzbgnXdpwaPDNuCdd2nBo3NtpGEqd9rDE52e3pxYHpysFmwsIG7g3Dcz-mMjmRty&_nc_ohc=HdVZjXoCKfoQ7kNvgFEzR9a&_nc_ht=scontent.ftbs5-3.fna&oh=00_AYB6SkxzugX5c_ioKSs-kmrxCo7NsLvGA3hyy2muhLTZgQ&oe=6673C3D7" alt="Logo" style="max-width: 150px; margin-bottom: 20px;">
           <h2>Thank you for contacting us, one of our team members will replay as soon as possible. </h2>
           <p style="margin-top: 20px;">If you did not sent this, please ignore this email</p>
         </div>
