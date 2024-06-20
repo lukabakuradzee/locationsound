9d3e8bf23bc735ff206558b3ea789b58bd692cfa
lukabakuradzee <lukabakuradzee@gmail.com>
2024-06-20 19:53:17 +0400

    updated version

--- backend/userService/sendEmail.js
+++ backend/userService/sendEmail.js
@@ -12,21 +12,39 @@ const sendEmail = async (req, res) => {
       },
     });
 
-    const mailOptions = {
+    const CustomerMailOptions = {
       from: "lukabakurade39@gmail.com",
       to: email,
+      cc: "lukabakuradze39@gmail.com",
       subject: "Thank you for contacting us",
       html: `
-        <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
-        <div style="max-width: 600px; margin: auto; border: 1px solid #ddd; padding: 20px; border-radius: 5px;">
-          <img src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/416841832_893621152769709_750835283606735095_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEd4KCPqAenl6Hu9b5cp1ICwzbgnXdpwaPDNuCdd2nBo3NtpGEqd9rDE52e3pxYHpysFmwsIG7g3Dcz-mMjmRty&_nc_ohc=HdVZjXoCKfoQ7kNvgFEzR9a&_nc_ht=scontent.ftbs5-3.fna&oh=00_AYB6SkxzugX5c_ioKSs-kmrxCo7NsLvGA3hyy2muhLTZgQ&oe=6673C3D7" alt="Logo" style="max-width: 150px; margin-bottom: 20px;">
+        <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: black;">
+        <div style="max-width: 600px; margin: auto; padding: 20px; border-radius: 5px; color: #f0eb68">
+          <img src="https://i.imgur.com/yNbUvfl.png" alt="Logo" style="max-width: 400px; margin-bottom: 15px;">
           <h2>Thank you for contacting us, one of our team members will replay as soon as possible. </h2>
           <p style="margin-top: 20px;">If you did not sent this, please ignore this email</p>
         </div>
       </div>`,
     };
 
-    await transporter.sendMail(mailOptions);
+    const adminMailOptions = {
+      from: "lukabakuradze39@gmail.com",
+      to: "lukabakuradze39@gmail.com",
+      subject: "New Contact Form Submission",
+      html: `
+        <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: black;">
+          <div style="max-width: 600px; margin: auto; padding: 20px; border-radius: 5px; color: #f0eb68">
+            <h2>New Contact Form Submission</h2>
+            <p><strong>Name: </strong> ${name}</p>
+            <p><strong>Email: </strong> ${email}</p>
+            <p><strong>Message:</strong> ${textarea}</p>
+          </div>
+        </div>`,
+    };
+
+
+    await transporter.sendMail(CustomerMailOptions);
+    await transporter.sendMail(adminMailOptions);
     res.status(200).send("Email sent successfully");
   } catch (error) {
     console.error("Error occurred:", error);
